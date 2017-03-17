465bafa44f99c2809a719a1449bb8180cf3b44cf
spiritphyz <tonyle@ocf.berkeley.edu>
2017-03-16 19:48:58 -0700

    Write every() in es5

--- underbar/2017-03-13/src/underbar.js
+++ underbar/2017-03-13/src/underbar.js
@@ -274,6 +274,10 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+    return _.reduce(collection, function(memo, item) {
+      return memo && Boolean(iterator(item));
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
