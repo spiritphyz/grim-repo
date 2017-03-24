d7b87e2c0cc9b2d21ebf0728eedf8cf7af576500
spiritphyz <tonyle@ocf.berkeley.edu>
2017-03-23 20:22:52 -0700

    Implement delay() in es5

--- underbar/session-alpha/src/underbar.js
+++ underbar/session-alpha/src/underbar.js
@@ -445,6 +445,10 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = [].slice.call(arguments, 2);
+    return setTimeout(function() {
+      func.apply(null, args);
+    }, wait);
   };
 
 
